563cc9b06b23b9f584a04e74fd896baa0386a818
muralikrishnasai36@gmail.com <muralikrishnasai36@gmail.com>
2023-08-13 17:43:02 -0400

    updated prob 268

--- leetcode_fulltimes/2023/august/268_missing_number.cpp
+++ leetcode_fulltimes/2023/august/268_missing_number.cpp
@@ -18,6 +18,19 @@ public:
 
         return res;
     }
+    
+    int missingNumberV1(vector<int>& nums) {
+
+        int si = nums.size();
+        long long res = 0;
+        long long tot = si*(si+1)/2; //you can also check if si is even/odd and accordingly divide by 2 first before multiplying
+
+        //for(vector<int>::iterator iter=nums.begin(), iter!=nums.end(); iter++) 
+        for(auto iter=nums.begin(); iter!=nums.end(); iter++) 
+        res += *iter;
+
+        return tot-res;
+    }
 };
 
 
@@ -27,6 +40,7 @@ int main() {
 
     vector<int> nums = {0,1,3};
     cout << "missing number: " << obj.missingNumber(nums) << endl;
+    cout << "missing number: " << obj.missingNumberV1(nums) << endl;
 
     return 0;
 }
